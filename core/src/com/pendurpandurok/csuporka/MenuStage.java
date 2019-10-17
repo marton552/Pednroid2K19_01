8e8bec48de350a70177bea33117a9a243be71b57
zltmmikls10 <02Zolika02>
2019-10-17 12:31:06 +0200

    Blur megvan az alfa cucc az valamiért rossz

--- core/src/com/pendurpandurok/csuporka/MenuStage.java
+++ core/src/com/pendurpandurok/csuporka/MenuStage.java
@@ -2,8 +2,13 @@ package com.pendurpandurok.csuporka;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -42,17 +47,122 @@ public class MenuStage extends MyStage {
     private final static float BOX_TO_WORLD = 1f;
     private final static float WORLD_TO_BOX = 1f / BOX_TO_WORLD;
 
+    //Blur
+    ShaderProgram blurShader;
+    ShaderProgram alphaTresholdShader;
+    FrameBuffer blurTargetA, blurTargetB;
+    TextureRegion fboRegion;
+
+    public static final int FBO_SIZE = 60;//1024;
+
+    public static final float MAX_BLUR = 2f;
+
+    final String VERT =
+            "attribute vec4 "+ ShaderProgram.POSITION_ATTRIBUTE+";\n" +
+                    "attribute vec4 "+ShaderProgram.COLOR_ATTRIBUTE+";\n" +
+                    "attribute vec2 "+ShaderProgram.TEXCOORD_ATTRIBUTE+"0;\n" +
+
+                    "uniform mat4 u_projTrans;\n" +
+                    " \n" +
+                    "varying vec4 vColor;\n" +
+                    "varying vec2 vTexCoord;\n" +
+
+                    "void main() {\n" +
+                    "	vColor = "+ShaderProgram.COLOR_ATTRIBUTE+";\n" +
+                    "	vTexCoord = "+ShaderProgram.TEXCOORD_ATTRIBUTE+"0;\n" +
+                    "	gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" +
+                    "}";
+
+    final String FRAG =
+            "#ifdef GL_ES\n" +
+                    "#define LOWP lowp\n" +
+                    "precision mediump float;\n" +
+                    "#else\n" +
+                    "#define LOWP \n" +
+                    "#endif\n" +
+                    "varying LOWP vec4 vColor;\n" +
+                    "varying vec2 vTexCoord;\n" +
+                    "\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    "uniform float resolution;\n" +
+                    "uniform float radius;\n" +
+                    "uniform vec2 dir;\n" +
+                    "const float smoothing = 1.0/16.0;\n" +
+                    "void main() {\n" +
+                    "	vec4 sum = vec4(0.0);\n" +
+                    "	vec2 tc = vTexCoord;\n" +
+                    "	float blur = radius/resolution; \n" +
+                    "    \n" +
+                    "    float hstep = dir.x;\n" +
+                    "    float vstep = dir.y;\n" +
+                    "    \n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x - 4.0*blur*hstep, tc.y - 4.0*blur*vstep)) * 0.05;\n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x - 3.0*blur*hstep, tc.y - 3.0*blur*vstep)) * 0.09;\n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x - 2.0*blur*hstep, tc.y - 2.0*blur*vstep)) * 0.12;\n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x - 1.0*blur*hstep, tc.y - 1.0*blur*vstep)) * 0.15;\n" +
+                    "	\n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x, tc.y)) * 0.16;\n" +
+                    "	\n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x + 1.0*blur*hstep, tc.y + 1.0*blur*vstep)) * 0.15;\n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x + 2.0*blur*hstep, tc.y + 2.0*blur*vstep)) * 0.12;\n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x + 3.0*blur*hstep, tc.y + 3.0*blur*vstep)) * 0.09;\n" +
+                    "	sum += texture2D(u_texture, vec2(tc.x + 4.0*blur*hstep, tc.y + 4.0*blur*vstep)) * 0.05;\n" +
+                    "   \n" +
+                    "   \n"+
+                    "	\n" +
+                    "   gl_FragColor = vec4(sum.rgb, 1);"+
+                    "}";
+
+    final String alphaVERT =
+                    "attribute vec4 "+ ShaderProgram.POSITION_ATTRIBUTE+";\n" +
+                    "attribute vec4 "+ShaderProgram.COLOR_ATTRIBUTE+";\n" +
+                    "attribute vec2 "+ShaderProgram.TEXCOORD_ATTRIBUTE+"0;\n" +
+
+                    "uniform mat4 u_projTrans;\n" +
+                    " \n" +
+                    "varying vec4 vColor;\n" +
+                    "varying vec2 vTexCoord;\n" +
+
+                    "void main() {\n" +
+                    "	vColor = "+ShaderProgram.COLOR_ATTRIBUTE+";\n" +
+                    "	vTexCoord = "+ShaderProgram.TEXCOORD_ATTRIBUTE+"0;\n" +
+                    "	gl_Position =  u_projTrans * " + ShaderProgram.POSITION_ATTRIBUTE + ";\n" +
+                    "}";
+    final String alphaFRAG = "#ifdef GL_ES\n" +
+            "#define LOWP lowp\n" +
+            "precision mediump float;\n" +
+            "#else\n" +
+            "#define LOWP \n" +
+            "#endif\n" +
+            "varying LOWP vec4 vColor;\n" +
+            "varying vec2 vTexCoord;\n" +
+            "\n" +
+            "uniform sampler2D u_texture;\n" +
+            "\n" +
+            "\n" +
+            "void main() {\n" +
+                "\n" +
+                "vec4 texColor = texture2D(u_texture, vTexCoord);\n" +
+                "\n" +
+                "//invert the red, green and blue channels\n" +
+                "texColor.a = 1.5 - texColor.a;\n" +
+                "if(texColor.a < 0.6) discard;\n" +
+                "\n" +
+                "gl_FragColor = texColor;\n" +
+            "}";
+
     private Sprite sprite;
+    OneSpriteStaticActor actor;
 
     public MenuStage(Batch batch, MyGame game) {
         super(new ExtendViewport(1920f / 32, 1080f / 32), batch, game);
 
-        OneSpriteStaticActor actor = new OneSpriteStaticActor(Assets.manager.get(Assets.BADLOGIC_TEXTURE));
+        actor = new OneSpriteStaticActor(Assets.manager.get(Assets.BADLOGIC_TEXTURE));
         actor.setSize(5, 5);
         //addActor(actor);
 
 
-        mWorld = new World(new Vector2(-9.2f, 0), false);
+        mWorld = new World(new Vector2(0, -9.2f), false);
         float width = getViewport().getWorldWidth();
         float height = getViewport().getWorldHeight();
 
@@ -69,8 +179,43 @@ public class MenuStage extends MyStage {
 
 
         mDebugRenderer = new Box2DDebugRenderer();
-        mParticleDebugRenderer = new ColorParticleRenderer(10000);
-        mParticleDebugRenderer.setMaxParticleNumber(mParticleSystem.getParticleCount() + 1000);
+        mParticleDebugRenderer = new ColorParticleRenderer(1000000);
+        mParticleDebugRenderer.setMaxParticleNumber(mParticleSystem.getParticleCount() + 10000);
+
+        ///////////////////////
+        //Create blur things //
+
+        ShaderProgram.pedantic = false;
+
+        blurShader = new ShaderProgram(VERT, FRAG);
+        if (!blurShader.isCompiled()) {
+            System.err.println(blurShader.getLog());
+            System.exit(0);
+        }
+        if (blurShader.getLog().length()!=0)
+            System.out.println(blurShader.getLog());
+
+
+        //setup uniforms for our shader
+        blurShader.begin();
+        blurShader.setUniformf("dir", 0f, 0f);
+        blurShader.setUniformf("resolution", FBO_SIZE);
+        blurShader.setUniformf("radius", 1f);
+        blurShader.end();
+
+        alphaTresholdShader = new ShaderProgram(alphaVERT, alphaFRAG);
+        if (!alphaTresholdShader.isCompiled()) {
+            System.err.println(alphaTresholdShader.getLog());
+            System.exit(0);
+        }
+        if (alphaTresholdShader.getLog().length()!=0)
+            System.out.println(alphaTresholdShader.getLog());
+
+
+        blurTargetA = new FrameBuffer(Pixmap.Format.RGBA8888, FBO_SIZE, FBO_SIZE, false);
+        blurTargetB = new FrameBuffer(Pixmap.Format.RGBA8888, FBO_SIZE, FBO_SIZE, false);
+        fboRegion = new TextureRegion(blurTargetA.getColorBufferTexture());
+        fboRegion.flip(false, true);
 
 
     }
@@ -121,16 +266,15 @@ public class MenuStage extends MyStage {
         systemDef.radius = 0.08f * WORLD_TO_BOX;
         systemDef.dampingStrength = 0.6f;
         systemDef.gravityScale = 1f;
-        systemDef.density = 2f;
 
         mParticleSystem = new ParticleSystem(mWorld, systemDef);
-        mParticleSystem.setParticleDensity(1f);
+        mParticleSystem.setParticleDensity(1000f);
 
         //Create a new particlegroupdefinition and set some properties
         //For the flags you can set more than only one
         mParticleGroupDef1 = new ParticleGroupDef();
         mParticleGroupDef1.color.set(1f, 0, 0, 1);
-        mParticleGroupDef1.flags.add(ParticleDef.ParticleType.b2_powderParticle);
+        mParticleGroupDef1.flags.add(ParticleDef.ParticleType.b2_waterParticle);
         mParticleGroupDef1.position.set(width * (30f / 100f) * WORLD_TO_BOX, height * (80f / 100f) * WORLD_TO_BOX);
 
         //Create a shape, give it to the definition and
@@ -141,6 +285,8 @@ public class MenuStage extends MyStage {
         //and how much are created
         PolygonShape parShape = new PolygonShape();
         parShape.setAsBox(width * (20f / 100f) * WORLD_TO_BOX / 2f, width * (20f / 100f) * WORLD_TO_BOX / 2f);
+        //parShape.setAsBox(10, 10);
+        //parShape.setRadius(1f);
         mParticleGroupDef1.shape = parShape;
         mParticleSystem.createParticleGroup(mParticleGroupDef1);
 
@@ -185,12 +331,83 @@ public class MenuStage extends MyStage {
         }
     }
 
+    void resizeBatch(int width, int height) {
+        getCamera().viewportWidth = width;
+        getCamera().viewportHeight = height;
+        getBatch().setProjectionMatrix(getCamera().combined);
+    }
+
     @Override
     public void draw() {
         mWorld.step(Gdx.graphics.getDeltaTime(), 10, 6, mParticleSystem.calculateReasonableParticleIterations(Gdx.graphics.getDeltaTime()));
 
         super.draw();
+
+        blurTargetA.begin();
+
+        //Clear the offscreen buffer with an opaque background
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //before rendering, ensure we are using the default shader
+        getBatch().setShader(null);
+
+        resizeBatch(FBO_SIZE, FBO_SIZE);
+        //resizeBatch(100, 100);
+
+
+        //now we can start drawing...
+        getBatch().begin();
+
+        //draw our scene here
+        //actor.draw(getBatch(), 1f);
         mParticleDebugRenderer.render(mParticleSystem, BOX_TO_WORLD, getCamera().combined);
+        //mDebugRenderer.render(mWorld, getCamera().combined);
+
+
+        getBatch().flush();
+        blurTargetA.end();
+
+        //now let's start blurring the offscreen image
+        getBatch().setShader(blurShader);
+
+        blurShader.setUniformf("dir", 1f, 0f);
+        float mouseXAmt = Gdx.input.getX() / (float)Gdx.graphics.getWidth();
+        //blurShader.setUniformf("radius", mouseXAmt * MAX_BLUR);
+        blurShader.setUniformf("radius",  MAX_BLUR);
+
+        //our first blur pass goes to target B
+        blurTargetB.begin();
+
+        //we want to render FBO target A into target B
+        fboRegion.setTexture(blurTargetA.getColorBufferTexture());
+
+        //draw the scene to target B with a horizontal blur effect
+        getBatch().draw(fboRegion, 0, 0);
+        getBatch().flush();
+
+        //finish rendering target B
+        blurTargetB.end();
+
+        resizeBatch(1920 / 32, 1080 / 32);
+        //resizeBatch(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        blurShader.setUniformf("dir", 0f, 1f);
+
+        float mouseYAmt = Gdx.input.getY() / (float)Gdx.graphics.getHeight();
+        //blurShader.setUniformf("radius", mouseYAmt * MAX_BLUR);
+        blurShader.setUniformf("radius", MAX_BLUR);
+
+        fboRegion.setTexture(blurTargetB.getColorBufferTexture());
+
+        getBatch().setShader(alphaTresholdShader);
+        getBatch().draw(fboRegion, 0, 0);
+        getBatch().setShader(null);
+        getBatch().end();
+
+        //mParticleDebugRenderer.render(mParticleSystem, BOX_TO_WORLD, getCamera().combined);
+
+        //render box2d
         mDebugRenderer.render(mWorld, getCamera().combined);
     }
 
