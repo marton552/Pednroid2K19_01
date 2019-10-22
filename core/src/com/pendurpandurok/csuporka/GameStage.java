fb07d8ad11836cac54625813b3a5c8ed31a2a613
Martin Horváth <pigfighter2@gmail.com>
2019-10-22 21:08:21 +0200

    Gamestagebe átraktam mindent sry, hogy megkéstem.

--- core/src/com/pendurpandurok/csuporka/GameStage.java
+++ core/src/com/pendurpandurok/csuporka/GameStage.java
@@ -1,15 +1,127 @@
 package com.pendurpandurok.csuporka;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import finnstr.libgdx.liquidfun.ColorParticleRenderer;
+import finnstr.libgdx.liquidfun.ParticleDef;
+import finnstr.libgdx.liquidfun.ParticleGroupDef;
+import finnstr.libgdx.liquidfun.ParticleSystem;
+import finnstr.libgdx.liquidfun.ParticleSystemDef;
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.MyStage;
 
 public class GameStage extends MyStage {
+
+    private World mWorld;
+    private ParticleSystem mParticleSystem;
+    private ColorParticleRenderer mParticleDebugRenderer;
+    private Box2DDebugRenderer mDebugRenderer;
+
+    private ParticleGroupDef mParticleGroupDef1;
+
+    RealisticWater rw;
+
+
     public GameStage(Batch batch, MyGame game) {
         super(new ExtendViewport(1920f / 32, 1080f / 32), batch, game);
+
+        rw = new RealisticWater(batch, getCamera(), 1920f / 32, 1080f / 32);
+
+
+        mWorld = new World(new Vector2(0, -9.2f), false);
+        float width = getViewport().getWorldWidth();
+        float height = getViewport().getWorldHeight();
+
+        createParticleStuff(width, height);
+
+        mDebugRenderer = new Box2DDebugRenderer();
+        mParticleDebugRenderer = new ColorParticleRenderer(mParticleSystem.getParticleCount() + 10000);
+    }
+
+    public void createRectangle(float posX, float posY, float width, float height, float angle) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.position.set(posX, posY);
+        bodyDef.angle = angle;
+
+        Body body = mWorld.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(width / 2.0f, height / 2.0f);
+        //System.out.println(height / 2.0f, width / 2.0f);
+
+        FixtureDef fixDef = new FixtureDef();
+        fixDef.friction = 0.8f;
+        fixDef.shape = shape;
+        body.createFixture(fixDef);
+
+        shape.dispose();
+    }
+
+    private void createParticleStuff(float width, float height) {
+        //First we create a new particlesystem and
+        //set the radius of each particle to 6 / 120 m (5 cm)
+        ParticleSystemDef systemDef = new ParticleSystemDef();
+        systemDef.radius = 0.2f;
+        systemDef.dampingStrength = 0.6f;
+
+        mParticleSystem = new ParticleSystem(mWorld, systemDef);
+        mParticleSystem.setParticleDensity(2f);
+
+        //Create a new particlegroupdefinition and set some properties
+        //For the flags you can set more than only one
+        mParticleGroupDef1 = new ParticleGroupDef();
+        mParticleGroupDef1.color.set(1f, 0, 0, 1);
+        mParticleGroupDef1.flags.add(ParticleDef.ParticleType.b2_waterParticle);
+        mParticleGroupDef1.position.set(width * (30f / 100f) * 1, height * (80f / 100f) * 1);
+
+        PolygonShape parShape = new PolygonShape();
+        parShape.setAsBox(2 / 2f, 10 / 2f);
+        //parShape.setAsBox(10, 10);
+        //parShape.setRadius(1f);
+        mParticleGroupDef1.shape = parShape;
+        //mParticleSystem.createParticleGroup(mParticleGroupDef1);
+
+    }
+
+    public void createParticles(float pX, float pY) {
+        mParticleGroupDef1.position.set(pX, pY);
+        mParticleSystem.createParticleGroup(mParticleGroupDef1);
+        updateParticleCount();
+    }
+
+    private void updateParticleCount() {
+        if(mParticleSystem.getParticleCount() > mParticleDebugRenderer.getMaxParticleNumber()) {
+            mParticleDebugRenderer.setMaxParticleNumber(mParticleSystem.getParticleCount() + 10);
+        }
+    }
+
+    @Override
+    public void draw() {
+        mWorld.step(Gdx.graphics.getDeltaTime(), 10, 6, mParticleSystem.calculateReasonableParticleIterations(Gdx.graphics.getDeltaTime()));
+
+
+        //draw our scene here
+        //actor.draw(getBatch(), 1f);
+
+        rw.startRender();
+        mParticleDebugRenderer.render(mParticleSystem, 1, getCamera().combined);
+        rw.stopRender();
+
+        //render box2d
+        mDebugRenderer.render(mWorld, getCamera().combined);
+
+        super.draw();
     }
 
     @Override
--- desktop/src/com/pendurpandurok/csuporka/desktop/DesktopLauncher.java
+++ desktop/src/com/pendurpandurok/csuporka/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.pendurpandurok.csuporka.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
